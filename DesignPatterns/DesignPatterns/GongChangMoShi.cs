0612f8b8fda847b6090290053a12f8246e383565
TVTL <88657494+TVTL@users.noreply.github.com>
2024-08-09 11:27:48 +0800

    Add files via upload

---
+++ DesignPatterns/DesignPatterns/GongChangMoShi.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// https://www.runoob.com/design-pattern/factory-pattern.html
+/// </summary>
+namespace DesignPatterns
+{
+    class GongChangMoShi
+    {
+    }
+    public interface Shape
+    {
+        void draw();
+    }
+    public class Rectangle:Shape
+    {
+        public void draw()
+        {
+            System.Console.WriteLine("Rectangle implements Shape::draw() method.");
+        }
+    }
+    public class Square : Shape
+    {
+        public void draw()
+        {
+            System.Console.WriteLine("Square implements Shape::draw() method.");
+        }
+    }
+    public class Circle : Shape
+    {
+        public void draw()
+        {
+            System.Console.WriteLine("Circle implements Shape::draw() method.");
+        }
+    }
+}
